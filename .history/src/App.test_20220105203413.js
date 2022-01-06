60a9dd12022c40a81783822353f5d0fd32311d47
Juan P Chica <juanpchica@hotmail.com>
2022-01-05 20:35:40 -0500

    finding by role

--- .history/src/App.test_20220105203413.js
+++ .history/src/App.test_20220105203413.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
+  const linkElement = screen.getByRole("link", { name: /learn reacts/i });
   expect(linkElement).toBeInTheDocument();
 });
